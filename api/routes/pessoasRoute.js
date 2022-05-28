7e3961cc25a174078ecbe796859f5c90588ffa90
Hiago Petris <Hiago.Petris@ibm.com>
2022-05-28 11:24:29 -0300

    Complete DB

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
+const { route } = require('express/lib/application')
 const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
 
 module.exports = router
